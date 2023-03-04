86bc7a0a7ce09ec3fbabe430eb6100da7f12ddfc
TheWolverine796 <pandeymrityunjay796@gmail.com>
2023-03-04 22:57:43 +0530

    removed fake for production env

--- server/server.js
+++ server/server.js
@@ -5,10 +5,6 @@ const data = require("./db.json");
 const isProductionEnv = process.env.NODE_ENV === "production";
 const server = jsonServer.create();
 
-// For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? clone(data) : "db.json", {
-  _isFake: isProductionEnv,
-});
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
