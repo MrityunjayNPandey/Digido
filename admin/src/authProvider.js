ceb157b54231bb70a7049fc4068ef57196fbb0a9
TheWolverine796 <pandeymrityunjay796@gmail.com>
2023-03-04 06:02:24 +0530

    dscs

--- admin/src/authProvider.js
+++ admin/src/authProvider.js
@@ -5,23 +5,21 @@ export const authProvider = (type, params) => {
   // when a user tries to log in
   if (type === AUTH_LOGIN) {
     const { username, password } = params;
-    const request = new Request("https://localhost:8000/auth/login", {
-      method: "POST",
-      body: JSON.stringify({ username, password }),
-      headers: new Headers({ "Content-Type": "application/json" }),
-    });
-    return fetch(request)
-      .then((response) => {
-        if (response.status < 200 || response.status >= 300) {
-          throw new Error(response.statusText);
-        }
-        return response.json();
-      })
-      .then(({ token }) => {
-        localStorage.setItem("token", token);
-      });
+    console.log(
+      params,
+      username,
+      password,
+      username == "digidosolutions.com",
+      password == "digidosolutions.com"
+    );
+    if (
+      username == "digidosolutions.com" &&
+      password == "digidosolutions.com"
+    ) {
+      localStorage.setItem("username", username);
+      return Promise.resolve();
+    }
   }
-  return Promise.resolve();
   // when a user tries to logout
   if (type === AUTH_LOGOUT) {
     localStorage.removeItem("username");
